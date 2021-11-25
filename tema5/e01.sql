8346dec0dbef48ac88a7be7847519fdf010d56cd
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-11-25 14:21:57 +0100

    Se ha hecho el primer apartado del ejercicio 2 del tema 5

--- tema5/e01.sql
+++ tema5/e01.sql
@@ -1,3 +1,9 @@
 SELECT department_name, MAX(salary)
 FROM employees JOIN departments USING(department_id)
+GROUP BY department_name;
+
+
+SELECT department_name, MIN(salary)
+FROM employees JOIN departments USING(department_id)
 GROUP BY department_name
+HAVING MAX(salary) < 5000;
