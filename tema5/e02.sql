7b58f3ab907ca4318f81874171caf499bf95e984
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2021-12-02 21:17:20 +0100

    He hecho el ejercicio 2 del tema 5

---
+++ tema5/e02.sql
@@ -0,0 +1,39 @@
+SELECT employee_id, count(order_id)
+FROM orders
+GROUP BY employee_id
+HAVING count(order_id) <= 150 AND count(order_id) >= 100;
+
+
+SELECT DISTINCT company_name
+FROM customers LEFT JOIN orders USING(customer_id)
+GROUP BY company_name
+HAVING COUNT(order_id) = 0;
+
+
+SELECT category_name, COUNT(order_id) "cantidad"
+FROM  categories JOIN products USING(category_id)
+		JOIN order_details USING(product_id)
+GROUP BY category_name
+ORDER BY cantidad DESC
+LIMIT 1;
+
+
+SELECT category_name, product_name, ROUND(SUM(order_details.unit_price*order_details.quantity*0.25)::numeric, 2)
+FROM categories JOIN products USING(category_id)
+		JOIN order_details USING(product_id)
+GROUP BY category_name, product_name;
+
+
+SELECT contact_name
+FROM shippers JOIN orders ON(shipper_id=ship_via)
+		JOIN customers USING(customer_id)
+WHERE shippers.company_name ILIKE 'United Package'
+GROUP BY contact_name;
+
+
+
+
+
+
+
+
