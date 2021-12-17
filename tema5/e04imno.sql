1ff3c978aec14afc97b5a58cb6acc3d61a0cf1ea
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-12-17 10:02:07 +0100

    Corregido el boletín 4

--- tema5/e04imno.sql
+++ tema5/e04imno.sql
@@ -10,6 +10,24 @@ WHERE precio_final >= ALL(
 							)
 ORDER BY provincia;
 
+--Correccion
+SELECT i1.provincia, comprador
+FROM inmueble i1 JOIN operacion USING(id_inmueble)
+		JOIN tipo ON(tipo_inmueble=id_tipo)
+		JOIN comprador USING(id_cliente)
+WHERE tipo.nombre = 'Piso'
+  AND tipo_operacion = 'Venta'
+  AND precio_final >= ALL(
+	  SELECT precio_final
+	  FROM inmueble i2 JOIN operacion USING(id_inmueble)
+			JOIN tipo ON(tipo_inmueble=id_tipo)
+	  WHERE tipo.nombre = 'Piso'
+	    AND tipo_operacion = 'Venta'
+	    AND i1.provincia = i2.provincia
+  							);
+
+
+
 
 SELECT provincia, comprador.nombre, precio_final, fecha_operacion, t1.nombre --No se que es la cuantía
 FROM operacion o1 JOIN inmueble i1 USING(id_inmueble)
@@ -23,3 +41,18 @@ WHERE precio_final <= ALL(
 	  AND EXTRACT(MONTH FROM o1.fecha_operacion) = EXTRACT(MONTH FROM o2.fecha_operacion)
 							)
 ORDER BY EXTRACT(MONTH FROM o1.fecha_operacion);
+
+
+--Correccion
+SELECT *
+FROM inmueble i1 JOIN operacion o1 USING(id_inmueble)
+		JOIN tipo ON(tipo_inmueble=id_tipo)
+WHERE tipo_operacion = 'Alquiler'
+  AND precio_final <= ALL (
+	  SELECT precio_final
+	  FROM inmueble i2 JOIN operacion o2 USING(id_inmueble)
+	  		JOIN tipo ON(tipo_inmueble=id_tipo)
+	  WHERE tipo_operacion = 'Alquiler'
+	    AND i1.provincia = i2.provincia
+	  	AND EXTRACT(MONTH FROM o1.fecha_operacion) = EXTRACT(MONTH FROM o2.fecha_operacion)
+  							)
--- tema5/e04vuelos.sql
+++ tema5/e04vuelos.sql
@@ -9,8 +9,41 @@ WHERE age(llegada, salida) >= ALL(
 							)
 ORDER BY to_char(v1.salida, 'ID'), age(llegada, salida) DESC;
 
+--Correccion
+SELECT to_char(v1.salida, 'TMDay'), a1.nombre, a2.nombre, TO_CHAR(salida, 'YYYY:MM:DD:HH12:MI'), 
+		TO_CHAR(llegada, 'YYYY:MM:DD:HH12:MI')
+FROM vuelo v1 JOIN aeropuerto a1 ON(v1.desde=a1.id_aeropuerto)
+		JOIN aeropuerto a2 ON(v1.hasta=a2.id_aeropuerto)
+WHERE age(llegada, salida) >= ALL(
+	SELECT age(llegada, salida)
+	FROM vuelo v2
+	WHERE to_char(v1.salida, 'TMDay') = to_char(v2.salida, 'TMDay')
+									)
+ORDER BY to_char(v1.salida, 'ID');
 
-SELECT *
+
+
+SELECT 
 FROM cliente JOIN reserva USING(id_cliente)
 GROUP BY id_cliente
-HAVING 
+HAVING id_cliente = 0;
+
+--Correccion
+
+SELECT vuelo.*, COALESCE(precio * (1 - (descuento::numeric/100)), precio)
+FROM vuelo v1 JOIN aeropuerto a1 ON(desde=id_aeropuerto)
+WHERE 
+
+SELECT c2.nombre, a2.nombre, a2.ciudad, SUM(COALESCE(precio * (1 - (descuento::numeric/100)), precio)) "total"
+FROM vuelo v1 JOIN aeropuerto a2 ON(desde=id_aeropuerto)
+		JOIN reserva USING(id_vuelo)
+		JOIN cliente c2 USING(id_cliente)
+GROUP BY c2.nombre, a2.nombre, a2.ciudad
+HAVING SUM(COALESCE(precio * (1 - (descuento::numeric/100)), precio)) >= ALL(
+	SELECT SUM(COALESCE(precio * (1 - (descuento::numeric/100)), precio))
+	FROM vuelo v2 JOIN aeropuerto a3 ON(desde=id_aeropuerto)
+			JOIN reserva USING(id_vuelo)
+			JOIN cliente c3 USING(id_cliente)
+	WHERE a2.nombre = a3.nombre
+	GROUP BY c3.nombre, a3.nombre, a3.ciudad
+																				)
