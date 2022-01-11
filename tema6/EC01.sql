b4651036afc43b1ee241acf389521eaaa5d7582b
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-01-11 18:33:48 +0100

    Mejorado el último ejercicio

--- tema6/EC01.sql
+++ tema6/EC01.sql
@@ -1,32 +1,39 @@
-CREATE TABLE libro (
-	isbn			char(17),
-	titulo			text,
-	dni_autor		char(9),
-	cod_genero		smallint,
-	cod_editorial	smallint
-)
+--No Seleccionar las 5 primeras lineas a no ser que ya hayas creado las siguientes tablas
+DROP TABLE edicion;
+DROP TABLE libro;
+DROP TABLE autor;
+DROP TABLE editorial;
+DROP TABLE genero;
 
 CREATE TABLE autor (
-	dni 			char(9),
-	nombre			text,
+	dni 			char(9) PRIMARY KEY,
+	nombre			text NOT NULL,
 	Nacionalidad	char(56)
-)
-
-CREATE TABLE editorial (
-	cod_editorial	serial,
-	nombre			text,
-	direccion		text,
-	poblacion		text
-)
+);
 
 CREATE TABLE genero (
-	id_genero	smallserial,
-	nombre		text,
+	id_genero	smallserial PRIMARY KEY,
+	nombre		text NOT NULL,
 	descripcion	text
-)
+);
+
+CREATE TABLE editorial (
+	cod_editorial	serial PRIMARY KEY,
+	nombre			text NOT NULL,
+	direccion		text,
+	poblacion		text
+);
+
+CREATE TABLE libro (
+	isbn			char(17) PRIMARY KEY,
+	titulo			text NOT NULL,
+	dni_autor		char(9) NOT NULL REFERENCES autor,
+	cod_genero		smallint NOT NULL REFERENCES genero,
+	cod_editorial	smallint NOT NULL REFERENCES editorial 
+);
 
 CREATE TABLE edicion (
-	isbn				char(17),
+	isbn				char(17) PRIMARY KEY REFERENCES libro,
 	fecha_publicacion	date,
-	cantidad			integer
-)
+	cantidad			integer CHECK (cantidad > 0)
+);
