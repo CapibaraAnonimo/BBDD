a0f13b3f2c34bb2bbac737906511224575bc1d84
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-01-12 19:12:19 +0100

    Mejorado el ejercicio de ayer

--- tema6/EC01.sql
+++ tema6/EC01.sql
@@ -6,34 +6,44 @@ DROP TABLE editorial;
 DROP TABLE genero;
 
 CREATE TABLE autor (
-	dni 			varchar(9) PRIMARY KEY,
+	dni 			varchar(9),
 	nombre			text NOT NULL,
-	Nacionalidad	varchar(56)
+	Nacionalidad	varchar(56),
+	CONSTRAINT pk_autor PRIMARY KEY (dni)
 );
 
 CREATE TABLE genero (
-	id_genero	smallserial PRIMARY KEY,
+	id_genero	smallserial,
 	nombre		text NOT NULL,
-	descripcion	text
+	descripcion	text,
+	CONSTRAINT pk_genero PRIMARY KEY (id_genero)
 );
 
 CREATE TABLE editorial (
-	cod_editorial	serial PRIMARY KEY,
+	cod_editorial	serial,
 	nombre			text NOT NULL,
 	direccion		text,
-	poblacion		text
+	poblacion		text,
+	CONSTRAINT pk_editorial PRIMARY KEY (cod_editorial)
 );
 
 CREATE TABLE libro (
-	isbn			varchar(17) PRIMARY KEY,
+	isbn			varchar(17),
 	titulo			text NOT NULL,
-	dni_autor		char(9) NOT NULL REFERENCES autor,
-	cod_genero		smallint NOT NULL REFERENCES genero,
-	cod_editorial	smallint NOT NULL REFERENCES editorial 
+	dni_autor		char(9) NOT NULL,
+	cod_genero		smallint NOT NULL,
+	cod_editorial	smallint NOT NULL,
+	CONSTRAINT pk_libro PRIMARY KEY (isbn),
+	CONSTRAINT fk_autor FOREIGN KEY (dni_autor) REFERENCES autor ON DELETE RESTRICT,
+	CONSTRAINT fk_genero FOREIGN KEY (cod_genero) REFERENCES genero ON DELETE RESTRICT,
+	CONSTRAINT fk_editorial FOREIGN KEY (cod_editorial) REFERENCES editorial ON DELETE NO ACTION 
 );
 
 CREATE TABLE edicion (
-	isbn				char(17) PRIMARY KEY REFERENCES libro,
+	isbn				char(17),
 	fecha_publicacion	date,
-	cantidad			integer CHECK (cantidad > 0)
+	cantidad			integer,
+	CONSTRAINT pk_edicion PRIMARY KEY (isbn),
+	CONSTRAINT fk_libro FOREIGN KEY (isbn) REFERENCES libro ON DELETE RESTRICT,
+	CONSTRAINT cantidad_versiones CHECK (cantidad > 0)
 );
